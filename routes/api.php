5c169dd0af6e7093d3d1d5fd06ebb0e79d18cd5e
ALIM <alim201381@gmail.com>
2023-03-12 23:25:54 +0330

    init

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::controller(AuthController::class)->group(function (){
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
