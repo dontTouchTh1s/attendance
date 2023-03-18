302e77fe83ac2f2aea42fd776b8e60ec32aa03da
Ali Mashayekhi <alim201381@gmail.com>
2023-03-19 00:15:38 +0330

    Add sanctum middleware to endpoint routes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function (){
     Route::post('login', 'login');
     Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+    Route::post('logout', 'logout')->middleware('auth:sanctum');
+    Route::post('refresh', 'refresh')->middleware('auth:sanctum');
 });
