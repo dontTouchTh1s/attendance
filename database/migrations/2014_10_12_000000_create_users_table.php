c2ab8221d8e312973c5f5cecd6631b3e083a3f06
Ali Mashayekhi <alim201381@gmail.com>
2023-04-21 00:44:31 +0430

    change users table columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number', 11);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
