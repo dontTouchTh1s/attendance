4dcf7a67d579fdc6f5d911be50252673081a01e7
Ali Mashayekhi <alim201381@gmail.com>
2023-04-29 11:36:41 +0430

    fix: move foreign id to penalty_condition table

--- database/migrations/2023_04_28_095922_create_group_policies_table.php
+++ database/migrations/2023_04_28_095922_create_group_policies_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,6 @@ return new class extends Migration
             $table->string('name');
             $table->integer('max_leave_month')->comment('minutes');
             $table->integer('max_leave_year')->comment('minutes');
-            $table->foreignId('penalty_condition_id')->constrained();
             $table->timestamps();
         });
     }
