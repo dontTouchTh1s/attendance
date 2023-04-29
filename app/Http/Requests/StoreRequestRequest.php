e6a29c385d66df6baf36e4704d267c9f1dbbaa50
Ali Mashayekhi <alim201381@gmail.com>
2023-04-29 11:38:04 +0430

    Change faker locale to fa_IR

--- app/Http/Requests/StoreRequestRequest.php
+++ app/Http/Requests/StoreRequestRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\RequestType;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rules\Enum;
 
 class StoreRequestRequest extends FormRequest
 {
@@ -11,7 +15,7 @@ class StoreRequestRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +26,28 @@ class StoreRequestRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'employee_id' => 'required|integer|',
+            'type' => [new Enum(RequestType::class), 'required'],
+            'description' => 'required|string',
+            'leave_type' => 'required_unless:type,overtime',
+            'dates' => 'required_unless:type,optionalLeave|json',
+            'from_hour' => 'required_if:type,overtime|date',
+            'to_hour' => 'required_with:from-hour|date',
+            'min_days' => 'required_if:type,optionalLeave',
+            'max_days' => 'required_with:min-days|integer',
+            'from_date' => 'required_with:min-days|date',
+            'to_date' => 'required_with:min-days|date',
+            'interval' => 'required_with:min-days|integer',
+
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'message' => 'validation error',
+            'errors' => $validator->errors()
+        ], 422));
     }
 }
