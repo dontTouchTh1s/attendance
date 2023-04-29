a51c2aa2ddc81b8885005c04edf6c68eb728fa53
Ali Mashayekhi <alim201381@gmail.com>
2023-04-29 11:37:27 +0430

    Add relations to model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    public function groupPolicy(): BelongsTo
+    {
+        return $this->belongsTo(GroupPolicy::class);
+    }
+
+    public function manager(): HasOne
+    {
+        return $this->hasOne(Employee::class);
+    }
 }
