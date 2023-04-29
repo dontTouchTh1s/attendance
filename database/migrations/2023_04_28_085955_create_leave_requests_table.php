c4ce7c06c120ea6987ca01a62abbb775c7f0abc0
Ali Mashayekhi <alim201381@gmail.com>
2023-04-29 11:37:05 +0430

    Use create enum type for enum cols

--- database/migrations/2023_04_28_085955_create_leave_requests_table.php
+++ database/migrations/2023_04_28_085955_create_leave_requests_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Enums\LeaveRequestsType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('leave_requests', function (Blueprint $table) {
             $table->id();
             $table->json('dates');
-            $table->enum('type', []);
-            $table->enum('status', ['accepted', 'declined', 'pending', 'pending-for-admin']);
+            $table->enum('type', LeaveRequestsType::values());
+            $table->enum('status', ['accepted', 'declined', 'pending', 'pendingForAdmin']);
             $table->string('feedback')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
