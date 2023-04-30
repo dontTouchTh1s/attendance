e4d339e3f0e5c81836d562d3c8c7e6bc72d67c49
Ali Mashayekhi <alim201381@gmail.com>
2023-04-30 12:24:41 +0430

    no longer use ApiTokens

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -5,18 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Laravel\Sanctum\HasApiTokens;
 
 class Request extends Model
 {
-    use HasApiTokens;
     use HasFactory;
+
     protected $fillable = [
         'employee_id',
         'requestable_id',
         'requestable_type',
     ];
-    public function requestable():MorphTo{
+
+    public function requestable(): MorphTo
+    {
         return $this->morphTo();
     }
 
