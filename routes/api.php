659bcb178a6f34c57ad1537fdcc7a97b16686745
Ali Mashayekhi <alim201381@gmail.com>
2023-04-30 12:23:18 +0430

    Add requests routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\RequestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +16,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::controller(AuthController::class)->group(function (){
+Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout')->middleware('auth:sanctum');
     Route::post('refresh', 'refresh')->middleware('auth:sanctum');
 });
+
+
+Route::prefix('requests')->group(function () {
+    Route::controller(RequestController::class)->group(function () {
+        Route::post('create', 'store');
+        Route::post('/', 'index')->middleware('auth:sanctum');
+    });
+});
+
