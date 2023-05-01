43fb5c56e82e49624e73cd6bb4f6aff8b916c5c6
Ali Mashayekhi <alim201381@gmail.com>
2023-05-02 00:23:40 +0430

    fix responses on methods

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreRequestRequest;
 use App\Http\Requests\UpdateRequestRequest;
 use App\Models\LeaveRequest;
 use App\Models\Request;
+use Illuminate\Http\Response;
 
 class RequestController extends Controller
 {
@@ -15,20 +16,24 @@ class RequestController extends Controller
      */
     public function index()
     {
-        return Request::all()->toJson();
+        return response(Request::all()->each(function ($req) {
+            $req->requestable;
+        }));
     }
 
     /**
      * Store a newly created resource in storage.
+     * @param StoreRequestRequest $request
+     * @return Response
      */
-    public function store(StoreRequestRequest $request)
+    public function store(StoreRequestRequest $request): Response
     {
         $requestable = '';
         switch ($request->type) {
-            case 'leave':
+            case 'leave': //Creating leave request
                 $requestable = LeaveRequest::create([
                     'dates' => $request->dates,
-                    'leave_type' => $request->type,
+                    'leave_type' => $request->leave_type,
                     'status' => LeaveRequestStatus::Pending,
                     'description' => $request->description
                 ]);
@@ -40,29 +45,32 @@ class RequestController extends Controller
                 //'from_date' =>
                 break;
             case 'overtime':
+                //Create an OvertimeRequest
                 break;
         }
-
         $req = Request::create([
             'employee_id' => $request->employee_id,
             'requestable_id' => $requestable->id,
             'requestable_type' => $requestable::class
         ]);
-
-        return $req->requestable->toJson();
+        $req->requestable;
+        return response($req, 201);
 
     }
 
 
     /**
      * Display the specified resource.
+     * @param Request $request
+     * @return Response
      */
-    public function show(Request $request)
+    public function show(Request $request): Response
     {
-        //
+        $request->requestable;
+        return response($request, 200);
     }
 
-    /**
+    /*
      * Show the form for editing the specified resource.
      */
     public function edit(Request $request)
