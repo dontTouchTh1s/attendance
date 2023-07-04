5241d03e228f5172a128cef8e04f20b7cd9e63e2
Ali Mashayekhi <alim201381@gmail.com>
2023-07-04 09:01:58 +0330

    Group policy now have work times, create random work time in factory

--- database/factories/GroupPolicyFactory.php
+++ database/factories/GroupPolicyFactory.php
@@ -16,11 +16,15 @@ class GroupPolicyFactory extends Factory
      */
     public function definition(): array
     {
+        $startHour = ['07:00:00', '08:00:00'];
+        $endHour = ['15:00:00', '14:00:00'];
         $groupPoliciesNames = ['کارمندان', 'نگهبانی', 'نظافت', 'حساب داری', 'مدیران ارشد'];
         return [
             'name' => fake()->randomElement($groupPoliciesNames),
             'max_leave_month' => fake()->numberBetween(7, 15),
             'max_leave_year' => fake()->numberBetween(84, 180),
+            'work_start_hour' => array_rand($startHour),
+            'work_end_hour' => array_rand($endHour)
         ];
     }
 }
