a44cb05185c0910878dae329ed7b9e8e78b86f42
Ali Mashayekhi <alim201381@gmail.com>
2023-07-08 11:57:57 +0330

    init work place model

---
+++ app/Http/Requests/StoreWorkPlaceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreWorkPlaceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'lat' => 'required',
+            'lng' => 'required',
+            'radius' => 'required',
+            'name' => 'required|string',
+            'address' => 'required|string',
+
+        ];
+    }
+}
