24d0b5c6882dbbe5c30f4057e3eb05eea25de991
Ali Mashayekhi <alim201381@gmail.com>
2023-07-15 07:30:53 +0330

    Use a location json object instead of 2 lat,lng columns

--- database/migrations/2023_04_08_052815_create_work_places_table.php
+++ database/migrations/2023_04_08_052815_create_work_places_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,7 @@ return new class extends Migration
     {
         Schema::create('work_places', function (Blueprint $table) {
             $table->id();
-            $table->double('lat');
-            $table->double('lng');
+            $table->json('location');
             $table->double('radius');
             $table->string('name', 50);
             $table->text('address');
