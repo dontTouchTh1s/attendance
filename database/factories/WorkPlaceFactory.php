7533c7041927f88db1e1d79917734ef0ee1bcc18
Ali Mashayekhi <alim201381@gmail.com>
2023-07-15 07:26:41 +0330

    Update WorkPlaceFactory.php
    Use a location object instead of lat,lng

--- database/factories/WorkPlaceFactory.php
+++ database/factories/WorkPlaceFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Nette\Utils\Json;
+use Nette\Utils\JsonException;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WorkPlace>
@@ -13,14 +15,15 @@ class WorkPlaceFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     * @throws JsonException
      */
     public function definition(): array
     {
         return [
             'name' => fake()->company,
             'radius' => fake()->numberBetween(80, 110),
-            'lat' => fake()->randomFloat(9, 22, 24),
-            'lng' => fake()->randomFloat(9, 50, 52),
+            'location' => Json::encode(['lat' => fake()->randomFloat(9, 22, 24),
+                'lng' => fake()->randomFloat(9, 50, 52)]),
             'address' => fake()->address
         ];
     }
