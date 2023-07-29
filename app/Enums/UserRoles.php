6fb9cffbeae1405e57e155e16cbb19f7b6ee30c3
Ali Mashayekhi <alim201381@gmail.com>
2023-07-29 18:04:27 +0330

    init

---
+++ app/Enums/UserRoles.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use ArchTech\Enums\Values;
+
+enum UserRoles: string
+{
+    use Values;
+
+    case EAA = 'expertAdministrativeAffairs';
+    case MAA = 'managerAdministrativeAffairs';
+    case Employee = 'employee';
+    case Manager = 'manager';
+}
