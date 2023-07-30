f57d7085dc819a5746a03e034e3284bb8b1e9990
Ali Mashayekhi <alim201381@gmail.com>
2023-07-30 15:32:23 +0330

    resource to return objections

---
+++ app/Http/Resources/ObjectionResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ObjectionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'description' => $this->description,
+            'employee_name' => $this->attendanceLeave->employee->name,
+            'type' => $this->attendanceLeave->type,
+            'date' => $this->attendanceLeave->date,
+
+
+        ];
+    }
+}
