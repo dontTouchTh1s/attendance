4925bdac61f2a7d9f639f7ce55da7964bb0593aa
Ali Mashayekhi <alim201381@gmail.com>
2023-08-02 14:49:16 +0330

    CodeCleanUp

--- app/Models/WorkPlace.php
+++ app/Models/WorkPlace.php
@@ -4,19 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class WorkPlace extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        'lat',
-        'lng',
+        'location',
         'address',
         'name',
         'radius'
     ];
+
     public function groupPolicies(): HasMany
     {
         return $this->hasMany(GroupPolicy::class);
