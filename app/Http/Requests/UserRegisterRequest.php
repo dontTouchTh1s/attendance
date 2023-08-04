abccfe57645c0dbe79ad065d65e86d53c18a94ba
Ali Mashayekhi <alim201381@gmail.com>
2023-08-04 17:12:27 +0330

    remove name from request

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -24,7 +24,6 @@ class UserRegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ];
