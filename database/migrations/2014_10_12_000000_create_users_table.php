131205e0a6941de0265650951424c4e3b522222d
Ali Mashayekhi <alim201381@gmail.com>
2023-08-07 13:54:43 +0330

    Move roll table to users first migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\UserRoles;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('roll', UserRoles::values());
             $table->rememberToken();
             $table->timestamps();
         });
