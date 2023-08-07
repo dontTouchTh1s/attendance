73cb9657ff5ec7de5a43bc911eb421006d362a0a
Ali Mashayekhi <alim201381@gmail.com>
2023-08-07 13:55:07 +0330

    Use all method before where to prevent error

--- app/Http/Controllers/AttendanceLeaveController.php
+++ app/Http/Controllers/AttendanceLeaveController.php
@@ -50,7 +50,7 @@ class AttendanceLeaveController extends Controller
      */
     public function user()
     {
-        return AttendanceLeave::where('employee_id', '=', \Auth::user()->employee->id)->get();
+        return AttendanceLeave::all()->where('employee_id', '=', \Auth::user()->employee->id);
     }
 
 
