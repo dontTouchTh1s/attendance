55b9ca36c2654926d8bb2bc9e29fd6c6e5bb687b
Ali Mashayekhi <alim201381@gmail.com>
2023-08-10 10:46:21 +0330

    Move calculate leaveTime to model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -47,5 +48,46 @@ class Employee extends Model
     public function attendanceLeaves(): HasMany
     {
         return $this->hasMany(AttendanceLeave::class);
+    }
+
+    public function totalLeaveMonth($month): int
+    {
+        $requests = $this->requests->where('requestable_type', LeaveRequest::class);
+        $totalLeave = 0;
+
+        // Check for remain leave time in month
+        foreach ($requests as $item) {
+            $from_date = new Carbon($item->requestable->from_date);
+            $to_date = new Carbon($item->requestable->to_date);
+            if ($from_date->month == $month) {
+                $iFrom_hour = new Carbon($item->requestable->from_hour);
+                $iTo_hour = new Carbon($item->requestable->to_hour);
+                $hourWorkTime = $iTo_hour->diffInHours($iFrom_hour);
+                $diff = $from_date->diffInDays($to_date) * $hourWorkTime * 60;
+                $totalLeave += $diff;
+
+            }
+        }
+        return $totalLeave;
+    }
+
+    public function totalLeaveYear($year): int
+    {
+        $requests = $this->requests->where('requestable_type', LeaveRequest::class);
+        $totalLeave = 0;
+        // Check for remain leave time in year
+        foreach ($requests as $item) {
+            $from_date = new Carbon($item->requestable->from_date);
+            $to_date = new Carbon($item->requestable->to_date);
+            if ($from_date->year == $year) {
+                $iFrom_hour = new Carbon($item->requestable->from_hour);
+                $iTo_hour = new Carbon($item->requestable->to_hour);
+                $hourWorkTime = $iTo_hour->diffInHours($iFrom_hour);
+                $diff = $from_date->diffInDays($to_date) * $hourWorkTime * 60;
+                $totalLeave += $diff;
+            }
+
+        }
+        return $totalLeave;
     }
 }
